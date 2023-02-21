dbd800ce2311dae5e3544302cdd4c89d1a2bf3e3
KevinA-cpu <kevinanderson1410@gmail.com>
2023-02-21 16:09:56 +0700

    #initial commit

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./HomePage.module.scss";
+
+const HomePage = () => (
+  <div className={styles.HomePage} data-testid="HomePage">
+    <h1>HomePage component</h1>
+  </div>
+);
+
+export default HomePage;
