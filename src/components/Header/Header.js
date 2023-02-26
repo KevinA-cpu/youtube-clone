b1f5411dc2c08d9f50c39f4271b210a7cf5a1d0b
KevinA-cpu <kevinanderson1410@gmail.com>
2023-02-26 16:52:50 +0700

    #customizin searchbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./Header.module.scss";
 import MenuButton from "../MenuButton/MenuButton";
 import YoutubeLogo from "../YoutubeLogo/YoutubeLogo";
+import SearchBar from "../SearchBar/SearchBar";
 
 const Header = () => (
   <div data-testid="Header">
     <div className="flex items-center">
       <MenuButton />
       <YoutubeLogo />
+      <SearchBar />
     </div>
   </div>
 );
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,30 +1,51 @@
-import React from "react";
-import styles from "./SearchBar.module.scss";
-import { Input, Space } from "antd";
+import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import "./SearchBar.module.scss";
 
-const { Search } = Input;
+function SearchBar() {
+  const [query, setQuery] = useState("");
+  const [extended, setExtended] = useState(false);
 
-const suffix = (
-  <AudioOutlined
-    style={{
-      fontSize: 16,
-      color: "#1890ff",
-    }}
-  />
-);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Do something with the query, such as perform a search
+    console.log(query);
+  };
 
-const onSearch = (value) => console.log(value);
-
-const SearchBar = () => (
-  <div className={styles.SearchBar} data-testid="SearchBar">
-    <Search
-      placeholder="input search text"
-      enterButton="Search"
-      size="large"
-      suffix={suffix}
-      onSearch={onSearch}
-    />
-  </div>
-);
+  return (
+    <form onSubmit={handleSubmit} className="relative">
+      <div className="flex items-center">
+        {extended && (
+          <div className="border border-blue-300 rounded-l-full border-r-transparent w-10 h-8-5 flex items-center pl-4">
+            <FaSearch />
+          </div>
+        )}
+        <input
+          type="text"
+          placeholder="Search"
+          className={`border bg-white text-gray-800 rounded-full pl-3 pr-4 py-1 w-96 focus:outline-none focus:bg-white focus:text-gray-900
+          ${
+            extended
+              ? "border-l-transparent rounded-l-none border-blue-300"
+              : "border-slate-300"
+          } `}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onFocus={() => setExtended(true)}
+          onBlur={() => setExtended(false)}
+        />
+      </div>
+      <div
+        className={`border  rounded-r-full absolute inset-y-0 right-0 flex items-center pr-3 w-13 bg-slate-100 
+      ${extended ? "border-l-blue-300" : "border-slate-300"}`}
+      >
+        <div className="w-px h-full"></div>
+        <button type="submit" className="ml-3.5">
+          <FaSearch />
+        </button>
+      </div>
+    </form>
+  );
+}
 
 export default SearchBar;
